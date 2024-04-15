505e0fa63d7da678e39511e497592add3e40c5c9
Learra Alip <lnuesalip@gmail.com>
2024-04-15 08:51:44 -0500

    Created the about, work, and contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
   def home
     render :home
   end
+  def about
+    render :about
+  end
+  def work
+    render :work
+  end
+  def contact
+    render :contact
+  end
+
 end
